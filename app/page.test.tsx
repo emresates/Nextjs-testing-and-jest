f8e9d729524df02d85453b216709e5ff0cc3ffbd
Secenory <emresates@outlook.com>
2025-01-28 14:25:33 +0300

    findBy

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -17,15 +17,26 @@ it("should render button component without crashing", () => {
   expect(element).toBeInTheDocument();
 });
 
-it("should render suffix component without crashing", () => {
-  render(<Home suffix="Test" />);
+// it("should render suffix component without crashing", () => {
+//   render(<Home suffix="Test" />);
 
-  const element = screen.getByText("test", {
+//   const element = screen.getByText("test", {
+//     exact: false,
+//   });
+
+//   const emptyElement = screen.queryByText("No suffix");
+
+//   expect(emptyElement).not.toBeInTheDocument();
+//   expect(element).toBeInTheDocument();
+// });
+
+it("should render suffix component with findBy without crashing", async () => {
+  // State kısmı
+  render(<Home />);
+
+  const element = await screen.findByText("Ahmet", {
     exact: false,
   });
 
-  const emptyElement = screen.queryByText("No suffix");
-
-  expect(emptyElement).not.toBeInTheDocument();
   expect(element).toBeInTheDocument();
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Home({ suffix }: { suffix?: string }) {
+  const [state, setState] = useState<string | null>("Ahmet");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setState("Mehmet");
+    }, 300);
+  }, []);
+
   return (
     <div className="flex items-center justify-center h-screen w-screen">
       <p>Modern World</p>
 
       <button>Button</button>
 
+      {/* queryBy */}
       {/* <p>{suffix ? suffix : "No suffix"}</p> */}
 
-      {suffix && <p>{suffix}</p>}
-      {!suffix && <p>No suffix</p>}
+      {/* {suffix && <p>{suffix}</p>}
+      {!suffix && <p>No suffix</p>} */}
+
+      <p>{state}</p>
     </div>
   );
 }
